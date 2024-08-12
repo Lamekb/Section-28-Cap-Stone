afe1c1fd0e4124ac0e28fd1970e73cc7a0722e87
Lamek Berhane <lamekberhane@Lameks-MacBook-Air.local>
2024-08-12 17:21:54 -0400

    Add CSS styles

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=729482a32d7e822f3170b09f87013c11`;
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=43.77&lon=-79.46&appid=729482a32d7e822f3170b09f87013c11`;
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +17,7 @@ app.get("/", async (req, res) => {
       const result = await axios.get(URL);
       res.render("index.ejs", {
           weather: result.data.weather[0].main, 
+          description: result.data.weather[0].description,
         
       });
   } catch (error) {
